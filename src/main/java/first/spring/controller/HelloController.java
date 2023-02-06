474a4305d1501f86629444d33a1bd00b682df9ca
lch <ccik1653@gmail.com>
2023-02-06 16:11:28 +0900

    0206

--- src/main/java/first/spring/controller/HelloController.java
+++ src/main/java/first/spring/controller/HelloController.java
@@ -3,6 +3,8 @@ package first.spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -11,4 +13,37 @@ public class HelloController {
         model.addAttribute("data","hello !!");
         return "hello";
     }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(name = "name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody  //http 프로토콜 바디 부분에 데이터를 직접 넣어주겠다는 뜻 // HttpMessageConverter가 동작함 객체 : JsonConverter , 단순 문자 : StringConverter
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+
+        private String name;
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
 }
