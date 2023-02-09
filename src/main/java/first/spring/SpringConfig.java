78b3d4cb194e9fedcd48518683019b1e8e7e30f6
lch <ccik1653@gmail.com>
2023-02-09 16:58:13 +0900

    0209

--- src/main/java/first/spring/SpringConfig.java
+++ src/main/java/first/spring/SpringConfig.java
@@ -1,14 +1,27 @@
 package first.spring;
 
 
+import first.spring.repository.JdbcMemberRepository;
+import first.spring.repository.JdbcTemplateMemberRepository;
 import first.spring.repository.MemberRepository;
 import first.spring.repository.MemoryMemberRepository;
 import first.spring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+
+    private DataSource datasource;
+    @Autowired
+    public SpringConfig(DataSource datasource) {
+        this.datasource = datasource;
+    }
+
 
     @Bean
     public MemberService memberService(){
@@ -17,6 +30,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+
+        //return new JdbcMemberRepository(datasource);
+        return new JdbcTemplateMemberRepository(datasource);
     }
 }
